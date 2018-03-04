bd37195eb142208f086cfd5bb9d44f575b6b0246
GavRobbs <gnation15@gmail.com>
2018-03-04 18:39:18 -0500

    First and hopefully one of the last commits

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+//var cors = require("cors");
+var bodyParser = require("body-parser");
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//app.use(cors());
+
+app.get("/", (req,res) => {
+	return res.send("You are here");
+});
+
+app.post("/update", (req, res) => {
+	console.log(req.body.type + "\n" + JSON.stringify(req.body.data));
+	return res.json({result: "valid"});
+});
+
+app.listen(process.env.PORT || 3000, (req, res) => {
+	console.log("My server is running");
+})
