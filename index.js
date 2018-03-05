66dde99445b22d235d0ee46d0e125e973048e63f
GavRobbs <gnation15@gmail.com>
2018-03-04 19:30:20 -0500

    Built in the bulk of the to and from functionality

--- index.js
+++ index.js
@@ -8,15 +8,49 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+var colors= [false, false, false, false, false, false];
+var blink = false;
+var blink_delay = 0;
+var pattern_switch_delay = 0;
+var pattern_code = "";
+var display_type = "nopattern";
+
+function ClearEverything(){
+	colors= [false, false, false, false, false, false];
+	blink = false;
+	blink_delay = 0;
+	pattern_switch_delay = 0;
+	pattern_code = "";
+	display_type = "nopattern";
+}
+
 app.get("/", (req,res) => {
 	return res.send("You are here");
 });
 
 app.post("/update", (req, res) => {
-	console.log(JSON.stringify(req.body) + "\n");
-	console.log(JSON.stringify(req.body.data_out) + "\n");
-	console.log(req.body.type + "\n");
+	ClearEverything();
+	if(req.body.type == "nopattern"){
+		display_type = "nopattern";
+		colors = req.body.data_out.color_array;
+		blink = req.body.data_out.blink;
+		blink_delay = req.body.data_out.blink_delay;
+		return res.json({type: 0, colors: colors, blink: blink, blink_delay : blink_delay});
+	} else{
+		display_type = "pattern";
+		pattern_code = req.body.data_out.pattern_code;
+		pattern_switch_delay = req.body.data_out.pattern_switch_delay;
+		return res.json({type: 1, pcode: pattern_code, delay: pattern_switch_delay});
+	}
 	return res.json({result: "valid"});
+});
+
+app.get("/todo", (req, res) => {
+	if(display_type == "nopattern"){
+		return res.json({type: 0, colors: colors, blink: blink, blink_delay : blink_delay});
+	} else{
+		return res.json({type: 1, pcode: pattern_code, delay: pattern_switch_delay});
+	}
 });
 
 app.listen(process.env.PORT || 3000, (req, res) => {
